c3571ab7e65cd382b4fa7b3d10cce6eff2275814
Vladislav <k0tjaka@yandex.ru>
2019-09-28 22:28:20 +0300

    step 5.0

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-https://travis-ci.org/x0xl0ma/frontend-project-lvl1.svg?branch=master
+
 
 [![Build Status](https://travis-ci.org/x0xl0ma/frontend-project-lvl1.svg?branch=master)](https://travis-ci.org/x0xl0ma/frontend-project-lvl1)
 
-!https://travis-ci.org/x0xl0ma/frontend-project-lvl1.svg?branch=master!:https://travis-ci.org/x0xl0ma/frontend-project-lvl1
 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import readlineSync from 'readline-sync';
 
 export const greeting = () => console.log('Welcome to the Brain Games!');
+
 export const userName = () => readlineSync.question('May I have your name?');
+
+export const rules = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+export const randomNum = (num = 1000) => Math.floor(Math.random() * num);
+
+export const question = () => console.log('Question:'(randomNum());
+
+export const yourAnswer = () => readlineSync.question('Your answer: ');
+
+export const isEven = (num) => {
+ if (num % 2 === 0) {
+   return true;
+ } return false;
+};
+
+export const isCorrectAnswer = (isEven(question)) ? 'yes' : 'no';
+
+export const firstGame = () => {
+ for(let i = 0; i < 3; i += 1) {
+ if (isCorrectAnswer === yourAnswer) {
+  console.log('Correct!');
+ } else (isCorrectAnswer !== yourAnswer) {
+  return console.log(`${yourAnswer} is wrong answer ;(. Correct answer was ${isCorrectAnswer}.
+Let's try again, ${name}!`);
+ } return console.log(`Congratulations, ${name}!`);
+ }
+};  
+
 
