d3e73f61b5d8ad2cb96b43c9c307c67e7c17f23a
Vladislav <k0tjaka@yandex.ru>
2019-10-07 22:34:01 +0300

    IT WORKS

--- src/index.js
+++ src/index.js
@@ -5,21 +5,21 @@ export const greeting = () => console.log('Welcome to the Brain Games!');
 export const userName = () => readlineSync.question('May I have your name?');
 
  const rules = 'Answer "yes" if the number is even, otherwise answer "no".'; 
- const question = (num = 1000) => Math.floor(Math.random() * num);
- const isEven = num => num % 2 === 0;
+ const question = (num) => Math.floor(Math.random() * num);
+ const isEven = (num) => num % 2 === 0;
  const isCorrectAnswer = isEven(question) ? 'yes' : 'no';
 
-export const firstGame = (rules, question, isCorrectAnswer) => {
+export const firstGame = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name?');
   console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  console.log(rules);
 
   const rightAnswers = 3;
   let counter = 0;
 
   for(; counter < rightAnswers; counter += 1) {
-    console.log(`Question: ${question}`);
+    console.log(`Question: ${question(1000)}`);
     const yourAnswer = readlineSync.question('Your answer: ');
     
     if(yourAnswer !== isCorrectAnswer) {
