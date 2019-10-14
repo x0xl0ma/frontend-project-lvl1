a6f75097d076780979326a7674918d1d80159e15
Vladislav <k0tjaka@yandex.ru>
2019-10-14 21:06:50 +0300

    brain-even - works!

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -3,6 +3,5 @@
 import { greeting, userName, rules, firstGame } from '..';
 
 greeting();
-const name = userName();
-console.log(`Hello, ${name}!`);
+
 firstGame();
--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
 import readlineSync from 'readline-sync';
+import { cons, car, cdr, toString } from '@hexlet/pairs';
 
 export const greeting = () => console.log('Welcome to the Brain Games!');
 
 export const userName = () => readlineSync.question('May I have your name?');
 
- const rules = 'Answer "yes" if the number is even, otherwise answer "no".'; 
- const question = (num) => Math.floor(Math.random() * num);
- const isEven = (num) => num % 2 === 0;
- const isCorrectAnswer = isEven(question) ? 'yes' : 'no';
-
-export const firstGame = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${userName}!`);
-  console.log(rules);
-
-  const rightAnswers = 3;
-  let counter = 0;
-
-  for(; counter < rightAnswers; counter += 1) {
-    console.log(`Question: ${question(1000)}`);
-    const yourAnswer = readlineSync.question('Your answer: ');
-    
-    if(yourAnswer !== isCorrectAnswer) {
-      console.log(`${yourAnswer} is wrong answer ;(. Correct answer was ${isCorrectAnswer}.`);
-      console.log(`Let's try again, ${userName}!`);
-      break;
-    } console.log('Correct!');
-  }
-  if (counter === rightAnswers) {
-    console.log(`Congratulations, ${userName}!`);
-  }
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (value) => value % 2 === 0;
+const getRandom = (min, max) => Math.round(Math.random() * (max - min) + min);
+const getQuestionAnswer = () => {
+  const question = getRandom(1, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return cons(question, correctAnswer);
 };
 
+const correctAnswersCount = 3;
+export const firstGame = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(description);
+  console.log();
+  for (let i = 0; i < correctAnswersCount; i += 1) {
+    const questionAnswer = getQuestionAnswer();
+    const question = car(questionAnswer);
+    const correctAnswer = cdr(questionAnswer);
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer !== correctAnswer) {
+      console.log();
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+    console.log();
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
 
 
 
